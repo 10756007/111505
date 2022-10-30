d0eb87a473d5c02a9f0c4fb4b69ad9a5dc831594
huangyoso <10756037@ntub.edu.tw>
2022-10-30 19:22:32 +0800

    1030node

--- testnode/index.js
+++ testnode/index.js
@@ -16,17 +16,45 @@ var con = mysql.createConnection({
   user: '111505',
   password: '@Imd505111',
   database: '111-Sushi',
-  /*prot :3306*/
+  prot: 3306
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("hi!");
+// });
+
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM Product", function (err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
 
 app.get('/', function (req, res) {
   res.render('index',{
-    game: '鮪魚壽司'
+    name1: '鮭魚壽司',
+    name2: '鮪魚壽司',
+    name3: '旗魚壽司',
+    name4: '鮮蝦壽司',
+    name5: '扇貝壽司',
+    name6: '鮭魚卵壽司',
+    name7: '蝦卵壽司',
+    name8: '稻荷壽司',
+    name9: '干貝壽司',
+    name10: '玉子燒壽司',
+    amount1: '18',
+    amount2: '17',
+    amount3: '15',
+    amount4: '14',
+    amount5: '14',
+    amount6: '12',
+    amount7: '12',
+    amount8: '11',
+    amount9: '9',
+    amount10: '8',
   });
 });
 
