41a21dc498f81c024dc1c7997e9ffb9a07c1fc63
10756037 <10756037@ntub.edu.tw>
2022-11-28 01:23:25 +0800

    1128_infield_screen

--- 111505-infield_screen/111505-infield_screen.js
+++ 111505-infield_screen/111505-infield_screen.js
@@ -20,42 +20,9 @@ var db_option =
   prot: 3306
   };
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM Product", function (err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
-
-// app.get('/', function (req, res) {
-//   res.render('111505-infield_screen',{
-//     name1: '鮭魚壽司',
-//     name2: '鮪魚壽司',
-//     name3: '旗魚壽司',
-//     name4: '鮮蝦壽司',
-//     name5: '扇貝壽司',
-//     name6: '鮭魚卵壽司',
-//     name7: '蝦卵壽司',
-//     name8: '稻荷壽司',
-//     name9: '干貝壽司',
-//     name10: '玉子燒壽司',
-//     amount1: '18',
-//     amount2: '17',
-//     amount3: '15',
-//     amount4: '14',
-//     amount5: '14',
-//     amount6: '12',
-//     amount7: '12',
-//     amount8: '11',
-//     amount9: '9',
-//     amount10: '8',
-//   });
-// });
-
 app.get('/', function (req, res) {
   var connection = mysql.createConnection(db_option);
-  var query = 'SELECT * FROM Amount order by Class';
+  var query = 'SELECT * FROM Amount order by quantity desc';
   connection.query(query, function(err, rows, fields){
     if(err) throw err;
     var data = rows;
